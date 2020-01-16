14ef49bcb760fc1e09daa6456a443a30d4a531d8
Praveen Perera <ppraveen25@gmail.com>
2020-01-16 17:12:39 -0500

    Refactor, extract to functions

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,10 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
+    let uid = get_uid();
+    enter_sudo();
+
     let (send, recv) = channel();
-
-    let uid_ouput = Command::new("id")
-        .arg("-u")
-        .output()
-        .expect("Unable to get uid");
-
-    let uid = String::from_utf8_lossy(&uid_ouput.stdout);
-
-    let _cmd = Command::new("sudo")
-        .arg("-i")
-        .output()
-        .expect("Unable to put user into sudo");
-
     let coundown_duration = 2;
     let mut time = coundown_duration;
 
@@ -30,21 +20,39 @@ fn main() {
     loop {
         println!("Starting self control in: {}", time);
 
-        let timer_ended = recv
-            .try_recv()
-            .map(|_reply| println!("Starting block now..."));
-
+        let timer_ended = recv.try_recv();
         if timer_ended.is_ok() {
-            let _ = Command::new("sudo")
-                .arg("/Applications/SelfControl.app/Contents/MacOS/org.eyebeam.SelfControl")
-                .arg(uid.to_string())
-                .arg("--install")
-                .output();
-
+            println!("Starting block now...");
+            start_self_control(uid);
             break;
         }
 
         thread::sleep(Duration::from_secs(1));
         time -= 1
     }
+}
+
+fn start_self_control(uid: String) {
+    Command::new("sudo")
+        .arg("/Applications/SelfControl.app/Contents/MacOS/org.eyebeam.SelfControl")
+        .arg(uid)
+        .arg("--install")
+        .output()
+        .expect("Unable to start self control");
+}
+
+fn get_uid() -> String {
+    let uid_ouput = Command::new("id")
+        .arg("-u")
+        .output()
+        .expect("Unable to get uid");
+
+    String::from_utf8_lossy(&uid_ouput.stdout).to_string()
+}
+
+fn enter_sudo() {
+    Command::new("sudo")
+        .arg("-i")
+        .output()
+        .expect("Unable to put user into sudo");
 }
