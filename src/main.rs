b1b54dd2141d6e046c571e33548ffb6348d837f9
Praveen Perera <ppraveen25@gmail.com>
2020-01-16 16:49:52 -0500

    Get uid using crate

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,41 @@
+use std::process::Command;
 use std::sync::mpsc::channel;
 use std::thread;
 use std::time::Duration;
+use users::get_current_uid;
 
 fn main() {
     let (send, recv) = channel();
 
-    let coundown_duration = 10;
+    let uid = get_current_uid();
+    let _cmd = Command::new("sudo")
+        .arg("-i")
+        .output()
+        .expect("Unable to put user into sudo");
+
+    let coundown_duration = 2;
     let mut time = coundown_duration;
 
-    // final
+    // timer
     thread::spawn(move || {
         thread::sleep(Duration::from_secs(coundown_duration));
         send.send(()).unwrap();
     });
 
     loop {
-        println!("Tick: {}", time);
+        println!("Starting self control in: {}", time);
 
-        let timer_ended = recv.try_recv().map(|_reply| println!("Done"));
+        let timer_ended = recv
+            .try_recv()
+            .map(|_reply| println!("Starting block now..."));
 
         if timer_ended.is_ok() {
+            let _ = Command::new("sudo")
+                .arg("/Applications/SelfControl.app/Contents/MacOS/org.eyebeam.SelfControl")
+                .arg(uid.to_string())
+                .arg("--install")
+                .output();
+
             break;
         }
 
