7da536054f2ecbd97049a5dc014eab9f164e7e45
Praveen Perera <ppraveen25@gmail.com>
2020-01-16 16:01:51 -0500

    Workout logic of countdown with two threads

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::sync::mpsc::channel;
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello world");
+    let (send_timer, recv_timer) = channel();
+    let (send, recv) = channel();
+
+    let coundown_duration = 10;
+    let mut time = coundown_duration.clone();
+
+    // timer
+    thread::spawn(move || loop {
+        thread::sleep(Duration::from_secs(1));
+        time = time - 1;
+        send_timer.send(time).unwrap();
+    });
+
+    // final
+    thread::spawn(move || {
+        thread::sleep(Duration::from_secs(coundown_duration));
+        send.send(()).unwrap();
+    });
+
+    loop {
+        let _ = recv_timer
+            .try_recv()
+            .map(|reply| println!("Tick: {}", reply));
+
+        let timer_ended = recv.try_recv().map(|_reply| println!("Done"));
+
+        if let Ok(_) = timer_ended {
+            break;
+        }
+
+        thread::sleep(Duration::from_millis(100));
+    }
 }
