96dbfee3fb9a4a8a59c691949c7b580945a0a2e2
Praveen Perera <ppraveen25@gmail.com>
2020-01-16 19:36:22 -0500

    Parse time before starting to options struct

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,47 @@
+use blocksoon::options::Options;
+use clap::{App, AppSettings, Arg};
 use std::process::Command;
 use std::sync::mpsc::channel;
 use std::thread;
 use std::time::Duration;
 
 fn main() {
+    let matches = App::new("BlockSoon")
+        .version(clap::crate_version!())
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .author("Praveen Perera <praveen@avencera.com>")
+        .about("\nStart SelfControl after your break")
+        .arg(
+            Arg::with_name("hours")
+                .long("hours")
+                .short("h")
+                .takes_value(true)
+                .help("Set amount of time to countdown in hours"),
+        )
+        .arg(
+            Arg::with_name("minutes")
+                .long("minutes")
+                .short("m")
+                .takes_value(true)
+                .help("Set amount of time to countdown in minutes"),
+        )
+        .arg(
+            Arg::with_name("seconds")
+                .long("seconds")
+                .short("s")
+                .takes_value(true)
+                .help("Set amount of time to countdown in seconds"),
+        )
+        .help_short("H")
+        .get_matches();
+
+    let options = Options::new_from_matches(&matches);
+
     let uid = get_uid();
     enter_sudo();
 
     let (send, recv) = channel();
-    let coundown_duration = 2;
+    let coundown_duration = options.seconds as u64;
     let mut time = coundown_duration;
 
     // timer
---
+++ src/options.rs
@@ -0,0 +1,34 @@
+use clap::ArgMatches;
+
+#[derive(Debug)]
+pub struct Options {
+    pub seconds: usize,
+}
+
+impl Options {
+    pub fn new_from_matches(matches: &ArgMatches) -> Options {
+        let (hours, minutes, seconds) = time_selector_tuple(matches);
+        let total_seconds = (hours * 60 * 60) + (minutes * 60) + seconds;
+
+        Options {
+            seconds: total_seconds,
+        }
+    }
+}
+
+fn time_selector_tuple(matches: &ArgMatches) -> (usize, usize, usize) {
+    (
+        matches
+            .value_of("hours")
+            .map(|v| v.parse().expect("Unable to parse"))
+            .unwrap_or(0),
+        matches
+            .value_of("minutes")
+            .map(|v| v.parse().expect("Unable to parse"))
+            .unwrap_or(0),
+        matches
+            .value_of("seconds")
+            .map(|v| v.parse().expect("Unable to parse"))
+            .unwrap_or(0),
+    )
+}
