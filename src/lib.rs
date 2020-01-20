ac4809df5f36a2fefc329a4f8f4997524809f26f
Praveen Perera <ppraveen25@gmail.com>
2020-01-20 13:04:33 -0500

    Refactor to separate library file

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,58 @@
 pub mod options;
 pub mod self_control;
+use chrono::Datelike;
+use notify_rust::Notification;
+use std::process::Command;
+
+pub fn enter_sudo() {
+    // refreshes sudo timestamp so we can still run SelfControl in sudo even
+    // after a long timer
+    Command::new("sudo")
+        .arg("-v")
+        .output()
+        .expect("Unable to put user into sudo");
+}
+
+pub fn start_self_control(uid: String) {
+    let _ = Notification::new()
+        .summary("BlockSoon")
+        .body("Starting SelfControl block now")
+        .show();
+
+    Command::new("sudo")
+        .arg("/Applications/SelfControl.app/Contents/MacOS/org.eyebeam.SelfControl")
+        .arg(uid)
+        .arg("--install")
+        .output()
+        .expect("Unable to start self control");
+
+    // clears sudo timestamp so that next sudo command will require password
+    Command::new("sudo")
+        .arg("-K")
+        .output()
+        .expect("Unable to remove sudo privileges");
+}
+
+pub fn get_uid() -> String {
+    let uid_ouput = Command::new("id")
+        .arg("-u")
+        .output()
+        .expect("Unable to get uid");
+
+    String::from_utf8_lossy(&uid_ouput.stdout).to_string()
+}
+
+pub fn check_if_already_running(options: &options::Options) -> () {
+    match &options.self_control {
+        Some(self_control) if self_control.block_started_date.year() != 4000 => {
+            let _ = Notification::new()
+                .summary("BlockSoon")
+                .body("SelfControl is already running")
+                .show();
+
+            println!("SelfControl is already running");
+            std::process::exit(1);
+        }
+        _ => (),
+    }
+}
